836c888145ab5f1625baae97a762f23cea835d73
Marat <marat@maratfm.local>
2010-02-09 14:31:48 +0300

    add caching to includes

--- includes/templatetags/includes_tags.py
+++ includes/templatetags/includes_tags.py
@@ -2,6 +2,7 @@
 from django import template
 from includes.models import Includes
 from django.utils.safestring import mark_safe
+from django.core.cache import cache
 register = template.Library()
 
 @register.simple_tag
@@ -9,7 +10,13 @@ def render_include(name):
     '''
     Тэг для вывода включения
     '''
-    
+    key = ('include_'+name).replace(' ', '_')    
+    content = cache.get(key)
+    if content: return content
+     
     obj, created = Includes.objects.get_or_create(name=name,
                   defaults={'content': name.capitalize()})
-    return mark_safe(obj.content)
+    content = mark_safe(obj.content)
+    
+    cache.set(key, content, 60*5)
+    return content
